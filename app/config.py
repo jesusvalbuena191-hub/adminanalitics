c5106fc56de7e15518c5ba88ed8515e3dfa111e9
jesusvalbuena191-hub <jesusvalbuena191@gmail.com>
2025-09-08 21:34:39 -0500

    Delete app directory

--- app/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/adminanalyticsdb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-      # DB adicional (registrosmintic para PR, IM, fases_dda_2)
-    SQLALCHEMY_BINDS = {
-        'registrosmintic': 'mysql+pymysql://root:@localhost/registrosmintic'
-    }
